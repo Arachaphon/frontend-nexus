7350e338d3b3c970f03022ca34b6675ecc8c854f
Arachaphon <arachaphonklinchuen@gmail.com>
2026-03-02 03:27:30 +0700

    feat: add requiredRole prop to redirect unauthorized users

--- src/hooks/ProtectedRoute.tsx
+++ src/hooks/ProtectedRoute.tsx
@@ -1,13 +1,24 @@
-
 import { Navigate } from 'react-router-dom'
+import { useAuth } from '../hooks/useAuth'
 
-export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
-    const token = localStorage.getItem('token')
-    const raw = localStorage.getItem('userSession')
+export default function ProtectedRoute({ 
+  children,
+  requiredRole 
+}: { 
+  readonly children: React.ReactNode
+  readonly requiredRole?: 'owner' | 'manager'
+}) {
+  const token = localStorage.getItem('token')
+  const raw = localStorage.getItem('userSession')
+  const { user } = useAuth()
 
-    if (!token || !raw) {
-        return <Navigate to="/login" replace />
-    }
+  if (!token || !raw) {
+    return <Navigate to="/login" replace />
+  }
 
-    return <>{children}</>
+  if (requiredRole && user?.role !== requiredRole) {
+    return <Navigate to="/homemain" replace />
+  }
+
+  return <>{children}</>
 }
