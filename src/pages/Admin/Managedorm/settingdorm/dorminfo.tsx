3a31e7d41ca4af733ff0649c47eeb05097a76f21
Arachaphon <arachaphonklinchuen@gmail.com>
2026-03-08 12:13:48 +0700

    fix:delete check svg in save dorminfo

--- src/pages/Admin/Managedorm/settingdorm/dorminfo.tsx
+++ src/pages/Admin/Managedorm/settingdorm/dorminfo.tsx
@@ -465,9 +465,6 @@ export default function DormInfo() {
                     </>
                   ) : (
                     <>
-                      <svg className="w-4 h-4" fill="none" stroke="currentColor" strokeWidth={2.5} viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
-                      </svg>
                       บันทึก
                     </>
                   )}
